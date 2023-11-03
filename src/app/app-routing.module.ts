ec5faede22ae1c7a0902b0264d382a6014a69e51
Larry González <larrygonzalez@icsred.com>
2023-11-03 12:09:47 -0300

    [page Ppal.]-Generate page and designe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './component/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"principal",component:PrincipalComponent},
+  {path:"**", component:PrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { PrincipalComponent } from './component/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
