0ced2b8f84e3362463667fd9ca81fbdadfe3c327
Larry González <larrygonzalez@icsred.com>
2023-11-03 12:14:44 -0300

    [pages components]- All components generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { PrincipalComponent } from './component/principal/principal.component';
+import { ServiciosComponent } from './component/servicios/servicios.component';
+import { NosotrosComponent } from './component/nosotros/nosotros.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    ServiciosComponent,
+    NosotrosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
